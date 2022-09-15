9f0090cf5a5416194b1402bbd0daca38591a0577
Leisan Latypova <latley90@gmail.com>
2022-09-15 15:26:44 +0300

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Leysan Latypova on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
